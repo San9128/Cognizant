ee5875649d149eed23db9a88f8496017a03b5a6a
Sanjna <140518730+San9128@users.noreply.github.com>
2024-08-08 12:55:12 +0530

    Create Scenario_2.sql

---
+++ Week 2/PL_SQL/EX_7/Scenario_2.sql
@@ -0,0 +1,66 @@
+CREATE OR REPLACE PACKAGE EmployeeManagement AS
+    PROCEDURE HireEmployee(
+        p_EmployeeID IN NUMBER,
+        p_Name IN VARCHAR2,
+        p_Position IN VARCHAR2,
+        p_Salary IN NUMBER,
+        p_Department IN VARCHAR2,
+        p_HireDate IN DATE
+    );
+
+    PROCEDURE UpdateEmployeeDetails(
+        p_EmployeeID IN NUMBER,
+        p_Name IN VARCHAR2,
+        p_Position IN VARCHAR2,
+        p_Salary IN NUMBER,
+        p_Department IN VARCHAR2
+    );
+
+    FUNCTION CalculateAnnualSalary(
+        p_EmployeeID IN NUMBER
+    ) RETURN NUMBER;
+END EmployeeManagement;
+/
+CREATE OR REPLACE PACKAGE BODY EmployeeManagement AS
+    PROCEDURE HireEmployee(
+        p_EmployeeID IN NUMBER,
+        p_Name IN VARCHAR2,
+        p_Position IN VARCHAR2,
+        p_Salary IN NUMBER,
+        p_Department IN VARCHAR2,
+        p_HireDate IN DATE
+    ) IS
+    BEGIN
+        INSERT INTO Employees (EmployeeID, Name, Position, Salary, Department, HireDate)
+        VALUES (p_EmployeeID, p_Name, p_Position, p_Salary, p_Department, p_HireDate);
+    END HireEmployee;
+
+    PROCEDURE UpdateEmployeeDetails(
+        p_EmployeeID IN NUMBER,
+        p_Name IN VARCHAR2,
+        p_Position IN VARCHAR2,
+        p_Salary IN NUMBER,
+        p_Department IN VARCHAR2
+    ) IS
+    BEGIN
+        UPDATE Employees
+        SET Name = p_Name,
+            Position = p_Position,
+            Salary = p_Salary,
+            Department = p_Department
+        WHERE EmployeeID = p_EmployeeID;
+    END UpdateEmployeeDetails;
+
+    FUNCTION CalculateAnnualSalary(
+        p_EmployeeID IN NUMBER
+    ) RETURN NUMBER IS
+        v_Salary NUMBER;
+    BEGIN
+        SELECT Salary INTO v_Salary
+        FROM Employees
+        WHERE EmployeeID = p_EmployeeID;
+
+        RETURN v_Salary * 12;  
+    END CalculateAnnualSalary;
+END EmployeeManagement;
+/
